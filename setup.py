45c802299fdcc3f31cfae262bad427476532e17e
Galo Maldonado <gmaldona@laas.fr>
2017-01-05 18:47:17 +0100

    tools for filtering and to compute algebra

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bmtools',
+    description='Biomechanics tools',
+    version='1.0',
+
+    packages=find_packages(exclude=['data','tests','examples']),
+    
+    author='Galo MALDONADO',
+    author_email='galo.maldonado@laas.fr',
+    
+    #install_requires = [
+    #    "pinocchio"
+    #],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
